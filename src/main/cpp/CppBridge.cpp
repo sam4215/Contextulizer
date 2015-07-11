99002ab91af630e055bcbd45a71ec68a562b7e19
sam4215 <sam4215mc@gmail.com>
2015-07-11 15:21:32 +0100

    Different approach to C++

--- src/main/cpp/CppBridge.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-int main(int argc, const char* argv[]) {
-  std::cout << "cpp tiein - bridge is active!" << endl;
-  return 0;
-}
--- src/main/cpp/CppMain.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
-int main(int argc, const char* argv[]) {
-  std::cout << "cpp tiein - main is active!" << endl;
-  return 0;
-}
